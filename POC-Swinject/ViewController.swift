610b73bf8c782fd3a7bc5d75e6d767dd9a03c05a
Ramon Honorio <ramon.honorio@miraeducacao.com.br>
2017-12-13 12:02:22 -0200

    Initial Commit

---
+++ POC-Swinject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POC-Swinject
+//
+//  Created by Ramon Honorio on 13/12/17.
+//  Copyright © 2017 Mira Educação. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
